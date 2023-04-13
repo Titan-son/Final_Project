a3433a052b4b630c789c96f4cc2fbc4d2e385e39
Titan-son <thalissondsv1@gmail.com>
2023-04-13 15:13:42 +0200

    ajouts des fonctions de filtre de all book

--- pages/Allbooks/AllBooks.js
+++ pages/Allbooks/AllBooks.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './AllBooks.module.css'
+import { useState } from 'react';
 
 
 export async function getStaticProps() {
@@ -22,60 +23,88 @@ export async function getStaticProps() {
     }
 }
 
-export default function AllBooks({data}) {
-    if (!data) {
-        return <div>Loading...</div>;
-    }
+export default function AllBooks({ data, allAuthors }) {
+    const [selectedGenre, setSelectedGenre] = useState('all');
+    const [searchQuery, setSearchQuery] = useState('');
+    const [selectedAuthor, setSelectedAuthor] = useState('');
+
+    const genres = ['all', 'Classics', 'Fantasy', 'Young Adult', 'Romance', 'Fiction',];
+
+    const filteredBooksByGenre =
+        selectedGenre === 'all'
+            ? data
+            : data.filter((book) => {
+                if (!book.genres) return false; // Si la propriété 'genres' n'existe pas, retournez 'false'
+                const bookGenres = book.genres.split(','); // Divisez la chaîne de caractères en un tableau de genres
+                return bookGenres.includes(selectedGenre.trim()); // Vérifiez si le genre sélectionné est présent dans le tableau de genres du livre
+            });
+
+    const filteredBooksBySearch = filteredBooksByGenre.filter((book) => {
+        if (!book.title) return false; // Si la propriété 'title' n'existe pas, retournez 'false'
+        return book.title.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+
+    const filteredBooks = selectedAuthor
+        ? filteredBooksBySearch.filter((book) => book.authors === selectedAuthor)
+        : filteredBooksBySearch;
+
     return (
         <div>
             <div className={styles.contain_all}>
 
 
-            <div className={styles.category_All_Book}>
-                <input type="search" />
+                <div className={styles.category_All_Book}>
+                    <input className={styles.input_search}
+                        type="search"
+                        placeholder="Search by title"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)} />
 
-                <div className={styles.div_category}>
-                    <div className={styles.Title}>
-                        <h2>Category</h2>
-                    </div>
-                    <div className={styles.category}>
-                        <p>all</p>
-                        <p>Classics</p>
-                        <p>Fantasy</p>
-                        <p>Young Adult</p>
-                        <p>Humor</p>
-                        <p>Fiction</p>
-                        <p>Inspirational</p>
-                    </div>
+                    <div className={styles.div_category}>
+                        <div className={styles.Title}>
+                            <h2>Category</h2>
+                        </div>
+                        <div className={styles.category}>
+                            {genres.map((genre, index) => (
+                                <p key={index} onClick={() => setSelectedGenre(genre)}>
+                                    {genre}
+                                </p>
+                            ))}
+                        </div>
 
-                    <div className={styles.Title}>
-                        <h2>Author</h2>
-                    </div>
-                    <select>
-                        <option></option>
-                    </select>
+                        <div className={styles.Title}>
+                            <h2>Author</h2>
+                        </div>
+                        <select value={selectedAuthor} onChange={(e) => setSelectedAuthor(e.target.value)}>
+                            <option value="">Select an author</option>
+                            {allAuthors.map((author, index) => (
+                                <option key={index} value={author}>
+                                    {author}
+                                </option>
+                            ))}
+                        </select>
 
-                    <div className={styles.Title}>
-                        <h2>Price</h2>
+                        <div className={styles.Title}>
+                            <h2>Price</h2>
+                        </div>
+
                     </div>
 
                 </div>
 
-            </div>
-
-            <div className={styles.card_book}>
-                {data.map((book, index) => (
-                    <div key={index} className={styles.book_card}>
-                        <img src={book.image_url} alt={book.title} className={styles.book_image} />
-                        <h3>{book.title}</h3>
-                        <p>{book.authors}</p>
-                        <p>Rating: {book.rating}/5</p>
-                    </div>
-                ))}
-            </div>
+                <div className={styles.card_book}>
+                    {filteredBooks.map((book, index) => (
+                        <div key={index} className={styles.book_card}>
+                            <img src={book.image_url} alt={book.title} className={styles.book_image} />
+                            <h3>{book.title}</h3>
+                            <p>{book.authors}</p>
+                            <p>Rating: {book.rating}</p>
+                        </div>
+                    ))}
+                </div>
 
             </div>
-            
+
 
         </div>
     )
